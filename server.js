8b90c5ac953535d4764ef37534662e1a36378ceb
v.kazarinov <v.kazarinov@cy.cardpay.com>
2018-12-13 13:30:43 +1000

    change port server and add horoku-postbuild to pkg

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+
 app.use(express.static(path.join(__dirname, 'build')));
+
 app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
-app.listen(3000);
+
+const port = process.env.PORT || 5000;
+
+app.listen(port);
